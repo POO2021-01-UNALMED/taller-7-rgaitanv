c089458d95dba4683f7ad917e865f3550255493b
rgaitanv <71475582+rgaitanv@users.noreply.github.com>
2021-04-26 20:04:07 -0500

    primer

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,57 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	
+
+	private String[] letras;
+	private String interpretacion;
+	
+
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	
+	public String interpretacion() {
+		return interpretacion;
+	}
+	
+	public String toString() {
+			return "ppp";
+	}
+
+
+	
+	
+	
+	
+	
+	public String[] getLetras() {
+		return letras;
+	}
+
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+
+	public void setInterpetracion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	
+}
